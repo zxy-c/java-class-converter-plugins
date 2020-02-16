48e94286f521a46cded9f4b962f22af4e25bec83
zxy <2544682494@qq.com>
2020-02-16 23:20:36 +0800

    demo

---
+++ Java Class to Typescript/demo/src/KotlinClass.ts
@@ -0,0 +1,19 @@
+interface KotlinClass{
+readonly name:string;
+readonly javaClass:JavaClass;
+body?:string
+}
+
+interface JavaClass{
+demo:string;
+number1:number;
+number2:number;
+aBoolean:boolean;
+list:Array<string>;
+array:Array<string>;
+customClass:CustomClass
+}
+
+interface CustomClass{
+a:string
+}
