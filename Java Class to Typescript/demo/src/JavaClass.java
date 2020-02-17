2c69227fbe975d67cc32659d68cbfd4575ca8c1d
2544682494@qq.com <2544682494@qq.com>
2020-02-17 17:36:34 +0800

    更新DEMO

--- Java Class to Typescript/demo/src/JavaClass.java
+++ Java Class to Typescript/demo/src/JavaClass.java
@@ -1,16 +1,19 @@
 import java.util.List;
+import java.util.Map;
 
 public class JavaClass {
     private String demo;
     private int number1;
     private long number2;
-    private boolean aBoolean;
+    private final Boolean aBoolean = true;
     private List<String> list;
     private String[] array;
     private CustomClass customClass;
+    private Map<String,CustomClass> map;
 }
 
 class CustomClass{
     private String a;
 
 }
+
