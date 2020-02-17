699888be14cabeacef8eeb86e519474c98498258
2544682494@qq.com <2544682494@qq.com>
2020-02-17 15:50:13 +0800

    修复无法格式化的错误

--- Java Class to Typescript/src/main/kotlin/com/zxy/ijplugin/javaToTypescript/actions/ClassToTypescriptFileAtClipboardAction.kt
+++ Java Class to Typescript/src/main/kotlin/com/zxy/ijplugin/javaToTypescript/actions/ClassToTypescriptFileAtClipboardAction.kt
@@ -3,13 +3,14 @@ package com.zxy.ijplugin.javaToTypescript.actions
 import com.intellij.ide.PsiCopyPasteManager
 import com.intellij.lang.javascript.psi.JSFile
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.codeStyle.CodeStyleManager
 import org.jetbrains.kotlin.idea.util.application.runWriteAction
 
 class ClassToTypescriptFileAtClipboardAction : AbstractClassToTypescriptFileAction() {
 
     override fun processFile(jsFile: JSFile, e: AnActionEvent) {
-        runWriteAction {
+        WriteCommandAction.runWriteCommandAction(jsFile.project){
             CodeStyleManager.getInstance(jsFile.project).reformat(jsFile)
         }
         PsiCopyPasteManager.getInstance().setElements(arrayOf(jsFile), true)
